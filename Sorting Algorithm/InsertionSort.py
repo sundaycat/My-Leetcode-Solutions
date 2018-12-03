0543fe835a41b5d637a3de9ad9fe1c64a08829bc
Hongzhe <hongzheliu11@gmail.com>
2018-12-02 17:38:57 -0800

    added new solutions

--- Sorting Algorithm/InsertionSort.py
+++ Sorting Algorithm/InsertionSort.py
@@ -1,14 +1,11 @@
 def insertion_sort(arr):
-    for j in range(1, len(arr)):
+    for j in range(1, len(arr), 1):
         i = j - 1
-        current = arr[j]
-        while i >= 0 and arr[i] > current:
+        while i >= 0 and arr[i + 1] < arr[i]:
             arr[i], arr[i + 1] = arr[i + 1], arr[i]
             i = i - 1
-        arr[i + 1] = current
 
-    return None
 
-arr = [3,4,1]
-insertion_sort(arr)
-print(arr)
+temp = [8, 2, 4, 9, 3, 6]
+insertion_sort(temp)
+print(temp)
